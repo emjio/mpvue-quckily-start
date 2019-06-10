8ebcd75a45011ff27837391af0aa4d50904cb415
chenmonster <1271246683@qq.com>
2019-06-10 15:51:27 +0800

    修复vuex数据不能正常储存的bug

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ const store = new Vuex.Store({
     storage: {
       getItem: key => wx.getStorageSync(key),
       setItem: (key, value) => wx.setStorageSync(key, value),
-      removeItem: key => wx.clearStorage()
+      removeItem: key => () => { console.log(1) }
     }
   })]
 })
